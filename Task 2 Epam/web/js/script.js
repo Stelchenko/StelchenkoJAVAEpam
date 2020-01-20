66c3cac49b50d2c9d509b595a86802805dd86d3a
-Arioh- <sanchezzasdjob@gmail.com>
2020-01-20 15:36:45 +0300

    реализован футер с фамилией  и датой

--- Task 2 Epam/web/js/script.js
+++ Task 2 Epam/web/js/script.js
@@ -2,7 +2,7 @@ window.onload = function(){
     (function(){
         var date = new Date();
         var time = date.getDate() + '/' + date.getMonth() + 1 + '/' +  date.getFullYear() + ' ' +  date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
-        document.getElementsByTagName('footer')[0].innerHTML = '&copy; Stelchenko A.V. ' + time;
+        document.getElementsByTagName('footer')[0].innerHTML = '&copy; Stelchenko A.V ' + time;
         window.setTimeout(arguments.callee, 1000);
     })();
 };
